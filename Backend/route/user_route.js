73c010a01a4d3fe0e3f582e64f17f5c3af67a596
AbhishekAnan00 <email.abhishek00@gmail.com>
2024-05-05 22:26:57 +0530

    Deleted files: Backend/DB/connectToMongoDB.js ... Fronted

--- Backend/route/user_route.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import protector from "../middleware/protector.js";
-import { userController } from "../controller/user_controller.js";
-
-const router = express.Router();
-
-router.get("/", protector,userController);
-
-export default router;
